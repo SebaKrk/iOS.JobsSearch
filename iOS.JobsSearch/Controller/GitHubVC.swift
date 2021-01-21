c16d0db1988bb534cdea32a2d0ec4f5bb6a3209f
SebaKrk <s.sciuba@gmail.com>
2021-01-21 11:59:15 +0100

    creat tabelView in GitHubVC

--- iOS.JobsSearch/Controller/GitHubVC.swift
+++ iOS.JobsSearch/Controller/GitHubVC.swift
@@ -9,15 +9,45 @@
 
 import UIKit
 
-var serwice = Service()
-
 class GitHubVC : UIViewController {
     
+    var tableView = UITableView()
+    var reuseIdentifier = "cell"
+    var serwice = Service()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-
+        setupTableView()
+        setupViewConstraints()
+        
+    }
+    
+    func setupTableView() {
+        tableView.rowHeight = 75
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+    }
+    func setupViewConstraints () {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 
+}
+extension GitHubVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
 }
